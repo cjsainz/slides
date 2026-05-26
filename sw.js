dd263297bb587237df35c692407d40398c7272d1
cjsainz <cjsainz@gmail.com>
2026-05-26 03:30:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
-const CACHE_NAME = 'coro-mcle-v2'; // <--- Cambiado de v1 a v2
+const CACHE_NAME = 'coro-mcle-v3';
 const ASSETS = [
   'index.html',
   'cantos.txt',
-  'icono-coro.png', // <--- Asegúrate de que esté listado aquí el icono
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
   'https://cdn.jsdelivr.net/gh/gitbrent/pptxgenjs@3.12.0/dist/pptxgen.bundle.js'
 ];
 
-// El resto del archivo se queda exactamente igual...
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
